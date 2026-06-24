a163715fdf7e3de91ec529f428caa97c355af13b
iMatheusPaula <imatheuspaula@gmail.com>
2026-06-24 10:59:39 -0300

    refactor: remove Prisma-generated Agent model after migrating to Drizzle ORM

--- apps/api/prisma/migrations/20251007022744_add_unique_identifier/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[identifier]` on the table `Channel` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Channel_identifier_key" ON "Channel"("identifier");
