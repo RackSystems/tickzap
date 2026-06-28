83ceb3fa80aaa6dd9de5779e6c6d939c975f54e8
iMatheusPaula <imatheuspaula@gmail.com>
2026-06-27 22:16:13 -0300

    refactor: api

---
+++ apps/api-legacy/src/index.ts
@@ -0,0 +1,31 @@
+import express, { Express } from 'express';
+import routes from './routes/api';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import ErrorHandler from "./app/middlewares/ErrorHandler";
+import { notFoundHandler } from "./app/middlewares/notFoundHandler";
+// import { initGlobalWebSocket } from './websocket';
+
+const app: Express = express();
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:3001'],
+  credentials: true,
+}));
+
+app.use('/', routes);
+
+app.use(notFoundHandler);
+app.use(ErrorHandler);
+
+const port = process.env.PORT ?? 3000;
+
+const server = app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+
+  //initialize websockets - global and by ticket
+  // initTicketWebSocket(server);
+  // initGlobalWebSocket(server);
+});
